9081c7d35d566f9e2a83a6b1923d0fe60957e2a1
joshuafarara <joshua.farara@gmail.com>
2026-02-20 22:02:46 -0500

    Changes to be committed: 	new file:   views/RosterManagerView.java

---
+++ views/RosterManagerView.java
@@ -0,0 +1,13 @@
+package views;
+
+import java.lang.classfile.Label;
+
+public class RosterManagerView {
+
+    Label title = new Label("Roster Manager");
+    Testfield selectRoster = new Textfield();
+    selectRoster.setPromptTest("Select a Roster"); //change to a dropdown menu, or list display
+
+    
+
+}
