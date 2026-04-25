644417da11c16b4e9ceba3053b8e6974c3b228d6
joshuafarara <joshua.farara@gmail.com>
2026-04-25 05:34:03 -0400

    Begin implementing the Activities class and ActivitesTest class. Created the class AppConsoleTest to run a console based test for all models in a full succint text based, console interactive program. This will mimic how the GUI will operate and will help in the project structure. Ensures the functionality of the application works across multiple models. Ease the transition to GUI full application. Allows for testing in multiple situations and edge cases. Also added a project dictionary.

--- src/models/Activities.java
+++ src/models/Activities.java
@@ -1,5 +1,23 @@
 package models;
 
-public class Activities {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Activities {
+// ability to use a roster and perform activities with present students
+	ArrayList<Student> present;
+	
+	
+	public Activities(){
+		
+	}
+//	public Activities(ArrayList<Student> present){
+//		present = new 
+//	}
+	
+	public Student getRandomStudentName(ArrayList<Student> present) {
+		Random rand = new Random();
+		int selectedStudent = rand.nextInt(present.size());
+		return present.get(selectedStudent);	
+	}
 }
---
+++ src/tests/ActivitiesTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import models.Activities;
+
+public class ActivitiesTest {
+
+	public static void main(String[] args) {
+		Activities activities = new Activities();
+	}
+
+}
